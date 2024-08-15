09563b10385479e67b140e233d2ffd7c27dc9a1c
dasda98 <wojniakjakub@gmail.com>
2024-08-15 12:10:05 +0200

    Default options

---
+++ lua/config/options.lua
@@ -0,0 +1,26 @@
+local opt = vim.opt
+
+
+opt.syntax      =   "enable"
+
+-- indention
+opt.tabstop     =   4
+opt.shiftwidth  =   4
+opt.softtabstop =   4
+opt.expandtab   =   true
+opt.autoindent  =   true
+opt.shiftround  =   true
+opt.smartindent =   true
+
+
+-- search
+opt.hlsearch    =   true
+opt.ignorecase  =   true
+opt.smartcase   =   true
+
+
+-- ui
+opt.cursorline  =   true
+opt.relativenumber =   true
+opt.cc          =   "80"
+
