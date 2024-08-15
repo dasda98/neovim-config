8fa65fdfb09bfa2f551e24f0d12b611b67c725b2
dasda98 <wojniakjakub@gmail.com>
2024-08-15 13:15:03 +0200

    Fix tokyonight theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,9 +1,11 @@
 return {
     {
     "folke/tokyonight.nvim",
-        lazy = true,
-        opts = {
-            style = "night",
-        },
+        lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
     }
 }
